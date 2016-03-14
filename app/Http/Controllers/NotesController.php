c13f9d0d63c1577ac89460d8b4f80fe1ac97c6cb
Dillon Chanis <dillonchanis92@gmail.com>
2016-03-14 08:35:25 -0400

    Updating notes controller to store new notes

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
+use App\Note;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
 class NotesController extends Controller
 {
-    //
+    public function store(Request $request, Todo $todos)
+    {
+      $note = new Note;
+      $note->body = $request->body;
+
+      $todos->notes()->save($note);
+
+      return back();
+    }
 }
