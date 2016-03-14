f8d2e32b6f5bad608bb4e2470d3792462dfab4d6
Dillon Chanis <dillonchanis92@gmail.com>
2016-03-14 08:15:57 -0400

    Adding title to the page and link back to index

--- resources/views/todo/show.blade.php
+++ resources/views/todo/show.blade.php
@@ -2,10 +2,13 @@
 
 @section('content')
 <div class="container">
+  <h2>{{ $todos->title }}</h2>
+  <hr>
   <ul>
-    @foreach ($todo->notes as $note)
+    @foreach ($todos->notes as $note)
       <li>{{ $note->body  }}</li>
     @endforeach
   </ul>
+  <a href="/todo/">Back To Index</a>
 </div>
 @stop
