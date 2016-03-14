4f24902b4f59a96b737e61a55e71f602ac4f02b8
Dillon Chanis <dillonchanis92@gmail.com>
2016-03-14 19:09:56 -0400

    Adding form to allow for creation of todos

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -3,11 +3,33 @@
 @section('content')
 
 <div class="container">
-  <h1>All To Do Lists</h1>
-  <hr>
-    @foreach ($todos as $todo)
-      <a href="/todo/{{ $todo->id }}">{{ $todo->title }}</a>
-    @endforeach
-</div>
+  <div class="row">
+    <div class="col-md-12">
+      <h1>All To Do Lists</h1>
+      <hr>
+        <ul class="list-item-group">
+          @foreach ($todos as $todo)
+            <li><a href="/todo/{{ $todo->id }}">{{ $todo->title }}</a></li>
+          @endforeach
+        </ul>
+    </div>
+  </div>
 
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <h3>Add a ToDo</h3>
+
+      <form method="POST" action="/todo/new">
+        {{ csrf_field() }}
+
+        <div class="form-group">
+          <textarea name="title" class="form-control"></textarea>
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary">Add ToDo</button>
+        </div>
+      </form>
+  </div>
+  </div>
+</div>
 @stop
