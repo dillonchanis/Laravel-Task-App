f2e27881200c4cb232f1961faa0994ceea90d9f1
Dillon Chanis <dillonchanis92@gmail.com>
2016-03-14 19:09:02 -0400

    Allowing users to fill in the title of a new ToDo instance

--- app/Todo.php
+++ app/Todo.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
+    protected $fillable = ['title'];
+
     public function notes()
     {
       return $this->hasMany(Note::class);
