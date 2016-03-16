b4f1d115c024dd4b60f7f2da02b0f7e2a064f03e
Dillon Chanis <dillonchanis92@gmail.com>
2016-03-15 23:54:41 -0400

    Adding user's profile to display ToDo lists created by them

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests;
+use App\User;
+use App\Todo;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -25,5 +27,14 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function showProfile(User $name, Todo $todos)
+    {
+
+      $id = $name->id;
+      $todos = Todo::where("user_id", "=", $id)->get();
+
+      return view('profile', compact('name', 'todos'));
     }
 }
