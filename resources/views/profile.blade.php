df6c943ce32ff3ea30a5f10b94eeffb5f96aeaa6
Dillon Chanis <dillonchanis92@gmail.com>
2016-03-21 08:53:43 -0400

    Providing quick changes to display the bio on the page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,12 +3,20 @@
 @section('content')
 <div class="container">
   <h1>{{ $name->name }}</h1>
-</div>
+
 
 <div class="row">
-  @foreach ($todos as $todo)
-    <li><a href="/todo/{{ $todo->id }}">{{ $todo->title }}</a></li>
-  @endforeach
+  <div class="col-md-9">
+    <p>{{ $profile->bio }}</p>
+  </div>
+</div>
+<div class="row">
+  <div class"col-md-6 col-md-offset-3">
+    @foreach ($todos as $todo)
+      <li><a href="/todo/{{ $todo->id }}">{{ $todo->title }}</a></li>
+    @endforeach
+  </div>
 </div>
 
+</div>
 @stop
