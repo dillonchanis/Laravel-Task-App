b890cdfbdacd89e2d3d26e9ba4d341c75e467a08
Dillon Chanis <dillonchanis92@gmail.com>
2016-03-22 22:04:03 -0400

    Adding more navbar links to dashboard and todo index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,8 +48,9 @@
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
-
                             <ul class="dropdown-menu" role="menu">
+                                <li><a href="/home"><i class="fa fa-btn fa-home"></i> Dashboard</a></li>
+                                <li><a href="/todo"><i class="fa fa-btn fa-list-alt"></i> All Todos</a></li>
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i> Logout</a></li>
                             </ul>
                         </li>
